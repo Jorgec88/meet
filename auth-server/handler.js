04ee07adf3ab648467c7e4c5399c20db925dd974
Jorgec88 <j.carralero88@gmail.com>
2024-02-14 15:14:11 -0500

    update

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -7,6 +7,7 @@ const SCOPES = [
 ];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = ['https://Jorgec88.github.io/meet/'];
+
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
   CLIENT_SECRET,
@@ -30,3 +31,34 @@ module.exports.getAuthURL = async () => {
     })
   };
 };
+
+module.exports.getAccessToken = aync (event) => {
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(response);
+    });
+  })
+      .then((results) => {
+        return {
+          statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+          },
+          body: JSON.stringify(results),
+        }
+      })
+      .catch((error) => {
+        return {
+          statusCode: 500,
+          body: JSON.stringify(error),
+        };
+      });
+};  
+
+  
