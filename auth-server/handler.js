7c4b79a91d7d8d82f619144c844f45149c761272
Jorgec88 <j.carralero88@gmail.com>
2024-03-11 16:45:03 -0400

    update get access token

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -32,34 +32,34 @@ module.exports.getAuthURL = async () => {
   };
 };
 
-//module.exports.getAccessToken = aync (event) => {
-//const code = decodeURIComponent(`${event.pathParameters.code}`);
+module.exports.getAccessToken = aync (event) => {
+const code = decodeURIComponent(`${event.pathParameters.code}`);
 
-//return new Promise((resolve, reject) => {
-//oAuth2Client.getToken(code, (error, response) => {
-//if (error) {
-//  return reject(error);
-// }
-// return resolve(response);
-//});
-//})
-//  .then((results) => {
-//  return {
-//  statusCode: 200,
-//headers: {
-//  'Access-Control-Allow-Origin': '*',
-//  'Access-Control-Allow-Credentials': true,
-//},
-// body: JSON.stringify(results),
-// }
-//})
-//.catch((error) => {
-//  return {
-//  statusCode: 500,
-// body: JSON.stringify(error),
-// };
-//});
-//};
+return new Promise((resolve, reject) => {
+oAuth2Client.getToken(code, (error, response) => {
+if (error) {
+  return reject(error);
+ }
+ return resolve(response);
+});
+})
+  .then((results) => {
+  return {
+  statusCode: 200,
+headers: {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Credentials': true,
+ },
+ body: JSON.stringify(results),
+ }
+})
+.catch((error) => {
+  return {
+  statusCode: 500,
+ body: JSON.stringify(error),
+  };
+ });
+};
 
 //module.exports.getCalendarEvents = async (event) => {
 //  const access_token = decodeURIComponent(
