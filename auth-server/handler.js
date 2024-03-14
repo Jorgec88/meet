76eba91d3874eb16a38a0e83296d0c690e9c09e9
Jorgec88 <j.carralero88@gmail.com>
2024-03-14 11:07:28 -0400

    Update handler.js
    update

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -32,112 +32,100 @@ module.exports.getAuthURL = async () => {
   };
 };
 
-module.exports.getAccessToken = aync (event) => {
-const code = decodeURIComponent(`${event.pathParameters.code}`);
+module.exports.getAccessToken = async (event) => {
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
 
-return new Promise((resolve, reject) => {
-oAuth2Client.getToken(code, (error, response) => {
-if (error) {
-  return reject(error);
- }
- return resolve(response);
-});
-})
-  .then((results) => {
-  return {
-  statusCode: 200,
-headers: {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Credentials': true,
- },
- body: JSON.stringify(results),
- }
-})
-.catch((error) => {
-  return {
-  statusCode: 500,
- body: JSON.stringify(error),
-  };
- });
+  return new Promise((resolve, reject) => {
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(response);
+    });
+  })
+    .then((results) => {
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify(results)
+      };
+    })
+    .catch((error) => {
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error)
+      };
+    });
 };
 
-//module.exports.getCalendarEvents = async (event) => {
-//  const access_token = decodeURIComponent(
-//  `${event.pathParameters.access_token}`
-//    );
-//  oAuth2Client.setCredentials({ access_token });
+module.exports.getCalendarEvents = async (event) => {
+  const access_token = decodeURIComponent(
+    `${event.pathParameters.access_token}`
+  );
+  oAuth2Client.setCredentials({ access_token });
 
-//  return new Promise((resolve,reject) => {
-// calendar.events.list(
-// {
-//  calendarId: CALENDAR_ID,
-// auth: oAuth2Client,
-// timeMin: new Date().toISOString(),
-// sigleEvents: true,
-// orderBy: "startTime",
-// },
-// (error, response) => {
-//  if (error) {
-//   reject(error);
-//} else {
-// resolve(response);
-//}
-// }
-//);
-//})
-// .then((results) => {
-//   return {
-//   statusCode: 200,
-// headers: {
-// 'Access-Control-Allow-Origin': '*',
-//   'Access-Control-Aloow-Credentials': true,
-//  },
-//  body: JSON.stringify({ events: results.data.items }),
-//  };
-// })
-//.catch((error) => {
-//  return {
-//  statusCode: 500,
-//headers: {
-//   'Access-Control-Allow-Origin': '*',
-//     },
-//   body: JSON.stringify(error),
-//  };
-// });
-//};
-
-
-
-
-
-
+  return new Promise((resolve, reject) => {
+    calendar.events.list(
+      {
+        calendarId: CALENDAR_ID,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        sigleEvents: true,
+        orderBy: 'startTime'
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+  })
+    .then((results) => {
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Aloow-Credentials': true
+        },
+        body: JSON.stringify({ events: results.data.items })
+      };
+    })
+    .catch((error) => {
+      return {
+        statusCode: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(error)
+      };
+    });
+};
 
 //const codeValue = document.getElementById('code');
 //      const getAccessToken = document.getElementById('getToken');
 //      const accessTokenElement = document.getElementById('accessToken');
-  //    const getToken =
-    //    'https://rlzj6od56b.execute-api.us-east-2.amazonaws.com/dev/api/token';
+//    const getToken =
+//    'https://rlzj6od56b.execute-api.us-east-2.amazonaws.com/dev/api/token';
 
 //      getAccessToken.onclick = function () {
-  //      let code = codeValue.value;
-    //    if (decodeURIComponent(code) === code) {
-      //    code = encodeURIComponent(codeValue.value);
-        //}
+//      let code = codeValue.value;
+//    if (decodeURIComponent(code) === code) {
+//    code = encodeURIComponent(codeValue.value);
+//}
 //        const getTokenRequest = getToken + '/' + code;
 //        fetch(getTokenRequest)
-  //        .then(function (response) {
-    //        return response.json();
-      //    })
-        //  .then(function (jason) {
-   //         accessTokenElement.innerText = JSON.stringify(json);
-     //     });
-    //  }; 
-
-
-
-
-
-
+//        .then(function (response) {
+//        return response.json();
+//    })
+//  .then(function (jason) {
+//         accessTokenElement.innerText = JSON.stringify(json);
+//     });
+//  };
 
 //    <h4>Step 2: Get your code and exchange for an access token</h4>
 //      <p>
