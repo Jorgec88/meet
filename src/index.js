7bb8da2cdd87938030bb9e649994b7394369cedb
Jorgec88 <j.carralero88@gmail.com>
2024-03-29 18:10:26 -0400

    update

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('43d00761420041f797c3f4db896289d2').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
